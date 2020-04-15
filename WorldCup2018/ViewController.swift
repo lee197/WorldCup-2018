8d1900da6a8c178f5b1be52e562bd76fb5634966
Lee <advisor.ireland.web@gmail.com>
2020-04-15 11:08:41 +0100

    Initial Commit

---
+++ WorldCup2018/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldCup2018
+//
+//  Created by 李祺 on 15/04/2020.
+//  Copyright © 2020 Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorldCup2018Tests/WorldCup2018Tests.swift
@@ -0,0 +1,34 @@
+//
+//  WorldCup2018Tests.swift
+//  WorldCup2018Tests
+//
+//  Created by 李祺 on 15/04/2020.
+//  Copyright © 2020 Lee. All rights reserved.
+//
+
+import XCTest
+@testable import WorldCup2018
+
+class WorldCup2018Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
