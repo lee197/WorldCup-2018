f8f215acc4b077bf8ddac291d54dba5507f2e11c
Lee <114220313@umail.ucc.ie>
2020-04-17 17:44:04 +0100

    fixed image resizing bugs

--- WorldCup2018/View/TeamFlagImageCell.swift
+++ WorldCup2018/View/TeamFlagImageCell.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class TeamFlagImageCell: UITableViewCell {
-    let flageImageView: UIImageView = {
-        let imageView = UIImageView()
+    let flageImageView: ScaleAspectFitImageView = {
+        let imageView = ScaleAspectFitImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView        
     }()
@@ -34,24 +34,3 @@ class TeamFlagImageCell: UITableViewCell {
     }
 }
 
-extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() {
-                self.image = image
-                self.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: image.size.height / image.size.width).isActive = true
-            }
-        }.resume()
-    }
-    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
-        guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
-    }
-}
